fcf0e5fc518430457ec4b28c0ed8eebdbe726673
jpynzon <joshuapauloynzon@gmail.com>
2024-10-18 17:31:26 +0800

    separated layout for admin and customer side, added lottiefile animation and 404 page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <Sidebar :isDrawerOpen="isDrawerOpen" @update:isDrawerOpen="toggleDrawer" />
-      <v-main>
-        <Header :isDrawerOpen="isDrawerOpen" @toggle-drawer="toggleDrawer" />
-      </v-main>
-      <router-view />
+      <component :is="currentLayout">
+        <router-view />
+      </component>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
+import AdminLayout from '@/layouts/AdminLayout.vue';
+import CustomerLayout from '@/layouts/CustomerLayout.vue';
 
-const isDrawerOpen = ref(false);
+const route = useRoute();
 
-const toggleDrawer = () => {
-  isDrawerOpen.value = !isDrawerOpen.value;
-};
+const currentLayout = computed(() => {
+  return route.path.startsWith('/admincms') ? AdminLayout : CustomerLayout;
+});
 </script>
