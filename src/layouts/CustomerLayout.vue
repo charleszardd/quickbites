ea34e384625ce5d10559c2990443423e2827787a
jpynzon <joshuapauloynzon@gmail.com>
2024-10-18 21:57:10 +0800

    fixed

--- src/layouts/CustomerLayout.vue
+++ src/layouts/CustomerLayout.vue
@@ -2,28 +2,18 @@
     <div>
         <Sidebar :isDrawerOpen="isDrawerOpen" @update:isDrawerOpen="toggleDrawer" />
         <Header :isDrawerOpen="isDrawerOpen" @toggle-drawer="toggleDrawer" />
-
         <v-container>
             <router-view />
         </v-container>
-
-
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { useRouter } from 'vue-router';
 
 const isDrawerOpen = ref(false);
-const router = useRouter();
 
 const toggleDrawer = () => {
     isDrawerOpen.value = !isDrawerOpen.value;
-};
-
-const logout = () => {
-
-    router.push('/auth/login');
 };
 </script>
--- src/pages/auth/Register.vue
+++ src/pages/auth/Register.vue
@@ -4,78 +4,33 @@
       <v-col cols="12" sm="8" md="4" class="login-col">
         <div class="text-h6 mb-5">Create an account</div>
         <v-form ref="formRef" v-model="isFormValid" @submit.prevent="register">
-          <v-text-field
-            v-model="form.firstName"
-            :rules="[rules.required]"
-            label="Enter your first name"
-            variant="outlined"
-            class="custom-radius"
-          ></v-text-field>
+          <v-text-field v-model="form.firstName" :rules="[rules.required]" label="Enter your first name"
+            variant="outlined" class="custom-radius"></v-text-field>
 
-          <v-text-field
-            v-model="form.lastName"
-            :rules="[rules.required]"
-            label="Enter your last name"
-            variant="outlined"
-            class="custom-radius"
-          ></v-text-field>
+          <v-text-field v-model="form.lastName" :rules="[rules.required]" label="Enter your last name"
+            variant="outlined" class="custom-radius"></v-text-field>
 
-          <v-text-field
-            v-model="form.email"
-            :rules="[rules.required, rules.email]"
-            type="email"
-            label="Enter your email address"
-            variant="outlined"
-            class="custom-radius"
-          ></v-text-field>
-          <v-text-field
-            v-model="form.phoneNumber"
-            type="number"
-            label="Enter your phone number (optional)"
-            variant="outlined"
-            class="custom-radius"
-          ></v-text-field>
-          <v-text-field
-            v-model="form.password"
-            :rules="[rules.required, rules.min]"
-            type="password"
-            required
-            label="Enter your password"
-            variant="outlined"
-            class="custom-radius"
-          ></v-text-field>
-          <v-text-field
-            v-model="form.confirmPassword"
-            :rules="[rules.required, confirmPasswordRule]"
-            required
-            type="password"
-            label="Enter your confirm password"
-            variant="outlined"
-            class="custom-radius"
-          ></v-text-field>
+          <v-text-field v-model="form.email" :rules="[rules.required, rules.email]" type="email"
+            label="Enter your email address" variant="outlined" class="custom-radius"></v-text-field>
+          <v-text-field v-model="form.phoneNumber" type="number" label="Enter your phone number (optional)"
+            variant="outlined" class="custom-radius"></v-text-field>
+          <v-text-field v-model="form.password" :rules="[rules.required, rules.min]" type="password" required
+            label="Enter your password" variant="outlined" class="custom-radius"></v-text-field>
+          <v-text-field v-model="form.confirmPassword" :rules="[rules.required, confirmPasswordRule]" required
+            type="password" label="Enter your confirm password" variant="outlined" class="custom-radius"></v-text-field>
 
           <div class="checkbox-container">
             <v-checkbox value="value"></v-checkbox>
-            <router-link class="terms-text" to="/terms"
-              >Terms and Condition</router-link
-            >
+            <router-link class="terms-text" to="/terms">Terms and Condition</router-link>
           </div>
-          
+
         </v-form>
-        <v-btn
-          type="submit"
-          @click="submitForm"
-          :loading="loading"
-          height="53"
-          class="button-text w-100 mt-1"
-          >Sign Up</v-btn
-        >
+        <v-btn type="submit" @click="submitForm" :loading="loading" height="53" class="button-text w-100 mt-1">Sign
+          Up</v-btn>
         <div class="mt-4">
           <p>
             Already have an account?
-            <router-link to="/auth/login" class="login-link"
-              >Log in</router-link
-            >
+            <router-link to="/auth/login" class="login-link">Log in</router-link>
           </p>
         </div>
       </v-col>
@@ -114,7 +69,7 @@ const rules = {
   required: (value) => !!value || "Required.",
   email: (value) => /.+@.+\..+/.test(value) || "E-mail must be valid.",
   min: (value) =>
-    value.length >= 10 || "Password must be at least 10 characters.",
+    value.length >= 8 || "Password must be at least 8 characters.",
 };
 
 const confirmPasswordRule = (value) =>
@@ -147,7 +102,6 @@ const register = async () => {
     window.$snackbar(`Please fill in all required fields!`, `error`);
   } finally {
     loading.value = false;
-    resetForm();
   }
 };
 
@@ -158,19 +112,22 @@ const submitForm = () => {
 };
 </script>
 <style scoped>
-.checkbox-container{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
+.checkbox-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
+
 .terms-text {
   color: #171826;
   align-items: center;
 }
+
 .button-text {
   background-color: #171826;
   color: white;
 }
+
 .login-link {
   text-decoration: underline;
   color: #171826;
