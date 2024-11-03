63ba54b50dd55c3bc094da900f229dab51293327
jpynzon <joshuapauloynzon@gmail.com>
2024-11-03 17:58:15 +0800

    time picker working

--- src/components/customer/CheckoutContent.vue
+++ src/components/customer/CheckoutContent.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="py-0 mt-0 pt-0">
     <v-row class="header-holder mt-1 mb-3">
-      <v-btn
-        to="/"
-        size="small"
-        class="items button-text"
-        variant="text"
-        color="black"
-      >
+      <v-btn to="/" size="small" class="items button-text" variant="text" color="black">
         <v-icon class="justify-start">mdi-arrow-left</v-icon>
       </v-btn>
       <h2 class="items">Checkout</h2>
@@ -17,15 +11,9 @@
       <h1 class="text-h6 font-weight-bold mt-4 mb-3">Pick-up options</h1>
       <v-row>
         <v-col cols="12">
-          <v-card
-            v-for="(option, index) in pickupOptions"
-            :key="index"
-            @click="selectPickupOption(index)"
-            :class="{ 'selected-card': selectedPickupOption === index }"
-            variant="outlined"
-            height="80px"
-            class="custom-radius d-flex align-center px-4 py-3 mb-3"
-          >
+          <v-card v-for="(option, index) in pickupOptions" :key="index" @click="selectPickupOption(index)"
+            :class="{ 'selected-card': selectedPickupOption === index }" variant="outlined" height="80px"
+            class="custom-radius d-flex align-center px-4 py-3 mb-3">
             <v-icon class="mr-3">{{ option.icon }}</v-icon>
             <v-col>
               <v-card-title class="px-0 py-0">{{ option.title }}</v-card-title>
@@ -40,19 +28,10 @@
       <h1 class="text-h6 font-weight-bold mt-4 mb-3">Select Payment Method</h1>
       <v-row>
         <v-col cols="12">
-          <v-card
-            v-for="(payment, index) in paymentMethods"
-            :key="index"
-            @click="selectPaymentMethod(index)"
-            :class="{ 'selected-card': activeIndex === index }"
-            variant="outlined"
-            height="70px"
-            class="custom-radius d-flex align-center px-4 py-3 mb-3"
-          >
-            <v-col
-              v-if="payment.title === 'Wallet'"
-              class="px-0 py-0 d-flex w-100 align-center"
-            >
+          <v-card v-for="(payment, index) in paymentMethods" :key="index" @click="selectPaymentMethod(index)"
+            :class="{ 'selected-card': activeIndex === index }" variant="outlined" height="70px"
+            class="custom-radius d-flex align-center px-4 py-3 mb-3">
+            <v-col v-if="payment.title === 'Wallet'" class="px-0 py-0 d-flex w-100 align-center">
               <v-icon class="mr-3">{{ payment.icon }}</v-icon>
 
               <v-card-title>{{ payment.title }}</v-card-title>
@@ -80,13 +59,7 @@
         </v-row>
 
         <v-col>
-          <v-btn
-            class="custom-radius w-100 mb-3"
-            height="50"
-            color="black"
-            :loading="loading"
-            @click="confirmOrder"
-          >
+          <v-btn class="custom-radius w-100 mb-3" height="50" color="black" :loading="loading" @click="confirmOrder">
             Confirm Order
           </v-btn>
         </v-col>
@@ -95,46 +68,23 @@
 
     <Terms />
 
-    <Modal
-      v-model="isScheduleModalVisible"
-      title="Schedule"
-      icon="mdi-calendar-clock"
-      height="300px"
-      width="350px"
-    >
-      <v-text-field
-        v-model="selectedTime"
-        variant="outlined"
-        label="Choose a time"
-        readonly
-        @click="showTimePickerModal = true"
-      ></v-text-field>
-
-      <v-btn color="black" height="55px" class="w-100" @click="confirmSchedule"
-        >Confirm</v-btn
-      >
+    <Modal v-model="isScheduleModalVisible" title="Schedule" icon="mdi-calendar-clock" max-width="500px">
+      <div class="d-flex justify-center">
+        <v-time-picker v-model="selectedTime" class="pt-0 mb-3" color="primary" />
+      </div>
+      <v-btn color="primary" height="50px" class="w-100" @click="confirmSchedule">Confirm</v-btn>
     </Modal>
 
-    <Modal
-      v-model="isConfirmationModalVisible"
-      title="Order placed!"
-      icon="mdi-check-circle"
-      height="300px"
-      width="345px"
-      class="mx-auto d-flex flex-row justify-space-between"
-    >
+    <Modal v-model="isConfirmationModalVisible" title="Order placed!" icon="mdi-check-circle" max-width="500px"
+      class="mx-auto d-flex flex-row justify-space-between">
       <div class="d-flex justify-space-between align-center">
-        <span class="font-weight-bold"
-          >Your order has been successfully placed.</span
-        >
+        <span class="font-weight-bold">Your order has been successfully placed.</span>
       </div>
       <div class="my-2">
         We'll notify you as soon as it's ready for pick up.
       </div>
-      <div class="mt-auto">
-        <v-btn @click="closeConfirmationModal" class="w-100" color="black"
-          >Okay</v-btn
-        >
+      <div class="mt-3">
+        <v-btn @click="closeConfirmationModal" class="w-100" height="50px" color="primary">Okay</v-btn>
       </div>
     </Modal>
   </div>
@@ -146,6 +96,7 @@ import { useRouter, useRoute } from "vue-router";
 import { cart } from "@/stores/cart";
 import axios from "axios";
 import { getAuth } from "@/pages/auth/authServiceProvider/authService";
+import { VTimePicker } from 'vuetify/labs/VTimePicker'
 
 const router = useRouter();
 const route = useRoute();
@@ -153,7 +104,7 @@ const totalPrice = ref(route.query.total);
 const selectedTime = ref("");
 const loading = ref(false);
 const customerData = ref({
-  balance:  null,
+  balance: null,
 });
 const activeIndex = ref(null);
 
@@ -180,7 +131,7 @@ onMounted(async () => {
     try {
       const response = await axios.get(`/api/customers/${customerId}`);
       customerData.value = response.data;
-      
+
     } catch (error) {
       console.error("Failed to fetch customer data:", error);
       window.$snackbar(
@@ -266,12 +217,12 @@ const confirmOrder = async () => {
     selectedPickupOption.value = false;
     activeIndex.value = false;
   } catch (error) {
-    if(error.response && error.response.status === 400){
+    if (error.response && error.response.status === 400) {
       window.$snackbar(`Low in stocks, order something else or try again later`, `error`);
       totalPrice.value = 0;
       selectedPickupOption.value = false;
       activeIndex.value = false;
-    } else{
+    } else {
       window.$snackbar(`Order failed! Please try again.`, "error");
       console.error(`Order failed:`, error);
     }
